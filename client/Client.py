7ab5ce9de8bffc4a0e8ad9a18a97180237453f1d
nerdlerss <nerdlerss@hotmail.com>
2019-05-21 18:57:01 +0800

    客户端文件

--- client/Client.py
+++ client/Client.py
@@ -4,29 +4,31 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 
 from common.Base import base 
+from common import User,File_contor
 
-class Agent(Base):
+from client import Views
+
+class Agent(base):
     def __init__(self):
-        super().__init__(Authenticator)
+        pass
 
+    def run(self):
+        '''
+            打印欢迎消息
+            获取用户名密码
+            等待用户输入
+        '''
+        view = Views.View()
+        view.display()
+        username,password = view.get_user_msg()        
+        user = User.User(username,password)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        while 1:
+            row_cmd = view.get_cmd()
+            #获取命令后发送到服务端
+    
 
 if __name__ == '__main__':
-    pass 
+    agent = Agent()
+    agent.run()
+    
