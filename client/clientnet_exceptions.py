95f00dc77ca2986814dcb32c87d7df61eeba760e
nerdlerss <nerdlerss@hotmail.com>
2020-06-30 11:31:36 +0800

    更换代码结构

--- client/clientnet_exceptions.py
+++
@@ -1,4 +0,0 @@
-class  Client_NetException(Exception):
-    def __init__(self,Errorcode):
-        super().__init__(self,Errorcode)
-        self.Errorcode = Errorcode
--- server/server_exceptions.py
+++
@@ -1,6 +0,0 @@
-
-class  InitExceptions(Exception):
-    def __init__(self,stepname):
-        super().__init__(stepname)
-        self.stepname = stepname
- 
